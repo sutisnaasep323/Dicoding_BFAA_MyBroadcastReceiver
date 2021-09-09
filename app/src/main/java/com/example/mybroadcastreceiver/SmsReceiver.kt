f49124bf8d5b28b26b2411a7b652eb7200d5f137
Asep Sutisna <sutisnaasep323@gmail.com>
2021-09-09 09:49:11 +0700

    menambahkan kode untuk menerima data dari pesan yang masuk

--- app/src/main/java/com/example/mybroadcastreceiver/SmsReceiver.kt
+++ app/src/main/java/com/example/mybroadcastreceiver/SmsReceiver.kt
@@ -3,11 +3,26 @@ package com.example.mybroadcastreceiver
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.telephony.SmsMessage
 
 class SmsReceiver : BroadcastReceiver() {
 
+    companion object {
+        private val TAG = SmsReceiver::class.java.simpleName
+    }
+
     override fun onReceive(context: Context, intent: Intent) {
-        // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
-        TODO("SmsReceiver.onReceive() is not implemented")
+
+    }
+
+    private fun getIncomingMessage(aObject: Any, bundle: Bundle): SmsMessage {
+        val currentSMS: SmsMessage
+        val format = bundle.getString("format")
+        currentSMS = if (Build.VERSION.SDK_INT >= 23){
+            SmsMessage.createFromPdu(aObject as ByteArray, format)
+        } else SmsMessage.createFromPdu(aObject as ByteArray)
+        return currentSMS
     }
 }
