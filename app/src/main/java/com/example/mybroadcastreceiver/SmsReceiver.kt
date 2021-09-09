376ee3c68c2b267f0172f5008f2f349435d1e35b
Asep Sutisna <sutisnaasep323@gmail.com>
2021-09-09 09:43:05 +0700

    Add class SmsReceiver

---
+++ app/src/main/java/com/example/mybroadcastreceiver/SmsReceiver.kt
@@ -0,0 +1,13 @@
+package com.example.mybroadcastreceiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class SmsReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
+        TODO("SmsReceiver.onReceive() is not implemented")
+    }
+}
