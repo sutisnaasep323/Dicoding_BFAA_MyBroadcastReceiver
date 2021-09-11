6dce521c93c1ec71370708cd1fa61b49e41cb0cb
Asep Sutisna <sutisnaasep323@gmail.com>
2021-09-11 21:14:54 +0700

    finish

--- app/src/main/java/com/example/mybroadcastreceiver/MainActivity.kt
+++ app/src/main/java/com/example/mybroadcastreceiver/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.example.mybroadcastreceiver
 
 import android.Manifest
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import com.example.mybroadcastreceiver.databinding.ActivityMainBinding
@@ -16,6 +21,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     private var binding: ActivityMainBinding? = null
+    private lateinit var downloadReceiver: BroadcastReceiver
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,20 +30,30 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         
         binding?.btnPermission?.setOnClickListener(this)
         binding?.btnDownload?.setOnClickListener(this)
+
+        downloadReceiver = object : BroadcastReceiver(){
+            override fun onReceive(context: Context?, intent: Intent?) {
+                Log.d(DownloadService.TAG, "Download Selesai")
+            }
+        }
+
+        val downloadIntentFilter = IntentFilter(ACTION_DOWNLOAD_STATUS)
+        registerReceiver(downloadReceiver, downloadIntentFilter)
     }
 
     override fun onClick(v: View) {
         when(v.id){
             R.id.btn_permission -> PermissionManager.check(this, Manifest.permission.RECEIVE_SMS, SMS_REQUEST_CODE)
             R.id.btn_download -> {
-
+                val downloadServiceIntent = Intent(this, DownloadService::class.java)
+                startService(downloadServiceIntent)
             }
         }
     }
 
     override fun onDestroy() {
         super.onDestroy()
-
+        unregisterReceiver(downloadReceiver)
         binding = null
     }
 
